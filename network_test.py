97e7e0e87beceed9c21913bee77d9c04bfdd8348
Bartek Bulat <bartek.bulat@gmail.com>
2012-06-14 01:22:18 +0200

    Some plotting

--- network_test.py
+++ network_test.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python2
+# coding=utf-8
 
 from ffnet import ffnet, mlgraph, savenet, loadnet
 from numpy import zeros, ones, argsort, array, size
+import pylab as pl
+import numpy as np
 from numpy.random import shuffle
 import voice_sample
 import re
@@ -22,7 +25,9 @@ def load_speaker_recognition_newtork(filename, create_new=False):
         network = loadnet(filename)
     except IOError, ex:
         if create_new:
+            
             network = ffnet(mlgraph((LPC_COE_NUM, people_num + LPC_COE_NUM,
+            #network = ffnet(mlgraph((LPC_COE_NUM, 10,
                 people_num)) )
     return (network, people, people_num)
 
@@ -55,20 +60,31 @@ def train_net_a_person(net, person, pn, word, rg):
     net.train_momentum(inputs, outputs)
     return net
 
-def learn_net(filename, word):
+def learn_net(filename, word, **args):
     (network, people, people_num) = load_speaker_recognition_newtork(filename, True);
 
     print "Loading voice samples..."
     samples = [ voice_sample.VoicePersonSamples(s) for s in people]
-    ans = raw_input("Select sample range (1-10):  ")
-    print ans
-    (rs, re) = map(lambda x: int(x), ans.strip().split("-"))
+
+    rs = args.get("rs",None)
+    re = args.get("re", None)
+
+    if rs is None or re is None:
+        ans = raw_input("Select sample range (1-10):  ")
+        (rs, re) = map(lambda x: int(x), ans.strip().split("-"))
     
     (inp, out) = get_inputs_and_outputs(samples, word, rs, re);
 
-    network.train_momentum(inp, out)
+    train_method = args.get("method", "momentum")
+    training = getattr(network, "train_" + train_method)
 
-    savenet(network, filename)
+    training(inp, out)
+
+    _save = args.get("save", True)
+
+    _save and savenet(network, filename)
+
+    return (network, samples)
 
 def test_net(filename, word):
     (network, people, people_num) = load_speaker_recognition_newtork(filename)
@@ -94,6 +110,58 @@ def test_net(filename, word):
         print "Wrong choice"
 
 def test_and_plot():
+    mean_reggresion = zeros((4,6))
+    pl.figure()
+    _words=[u"Imie", u"Nazwisko", u"Samochód", u"Kot"]
+
+    colors= ["#FF2800".lower(), "#FFB800".lower(), "#1729B0".lower(),
+            "#007929".lower()]
+    w = 3
+    p_names = ["Slope", "Intercept", "R-value", "P-value", "Slope Error",
+    "Estimation Error"]
+    for n, word in zip(range(4), voice_sample.WORDS):
+        fn = "{}_network_{}.net".format(word, voice_sample.timestamp() )
+        (network, samples) = learn_net(fn, word, rs=1, re=5, save=False)    
+        (inp, target) = get_inputs_and_outputs(samples, word, 6,10,False)
+        (output, reggresion) = network.test(inp, target, iprint=0)
+        mean_reggresion[n,:] = np.mean(reggresion,0)
+        x = np.arange(6)*6 + n
+        pl.bar(x, mean_reggresion[n,:], color=colors[n], width=0.9,
+                label=_words[n])
+    print mean_reggresion
+    pl.xticks( np.arange(6)*6+2, p_names )
+    pl.xlim( -1, 35 )
+    pl.legend()
+    pl.show()
+
+def test_different_methods():
+    mean_reggresion = zeros((5,6))
+    pl.figure()
+    word = "samochod"
+    metods = ["momentum", "tnc", "bfgs", "cg", "rprop"]
+    _words=[u"Backpropagation z momentem", u"Alg. BFGS (multicore)", 
+        u"Alg. BFGS", u"Gradient sprzezony", "Alg. RProp"]
+
+    colors= ["#FF2800".lower(), "#FFB800".lower(), "#1729B0".lower(),
+            "#007929".lower(), "#60016D"]
+    w = 3
+    p_names = ["Slope", "Intercept", "R-value", "P-value", "Slope Error",
+    "Estimation Error"]
+    for n, met in zip(range(5), metods):
+        fn = "{}_network_{}.net".format(met, voice_sample.timestamp() )
+        (network, samples) = learn_net(fn, word, rs=1, re=5, save=False,
+                method=met)    
+        (inp, target) = get_inputs_and_outputs(samples, word, 6,10,False)
+        (output, reggresion) = network.test(inp, target, iprint=0)
+        mean_reggresion[n,:] = np.mean(reggresion,0)
+        x = np.arange(6)*7 + n
+        pl.bar(x, mean_reggresion[n,:], color=colors[n], width=0.9,
+                label=_words[n])
+    print mean_reggresion
+    pl.xticks( np.arange(6)*7+2, p_names )
+    pl.xlim( -1, 44 )
+    pl.legend()
+    pl.show()
     pass
 
 def test_net_using_file(filename):
@@ -156,8 +224,8 @@ def main(argv):
     if action != 'plot' and not args:
         usage()
         return 2
-
-    filename = args[0]
+    elif len(args) > 0:
+        filename = args[0]
     
     
     if action == 'learn':
@@ -169,7 +237,8 @@ def main(argv):
     elif action == 'record':
         record_and_test(filename, word)
     elif action == 'plot':
-        test_and_plot()
+        test_different_methods();
+        #test_and_plot()
 
     return 0
 
